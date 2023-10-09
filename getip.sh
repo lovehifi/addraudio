1042b6afc62b8bca29349725a304b256e08b51c6
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-10-09 15:29:02 +0700

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -3,7 +3,8 @@
 # Check if the nfs line exists in /etc/fstab
 if ! grep -q "nfs" /etc/fstab; then
     echo "Adding default nfs line to /etc/fstab"
-    echo -e "11.22.33.44:/DSD  /mnt/MPD/NAS/DSD  nfs   defaults,noauto,bg,soft,timeo=5  0  0" >> /etc/fstab
+    sed -i '$!N;/\n$/!P;D' /etc/fstab # Remove trailing empty lines
+    echo -e "\n11.22.33.44:/DSD  /mnt/MPD/NAS/DSD  nfs   defaults,noauto,bg,soft,timeo=5  0  0" >> /etc/fstab
 else
     echo "NFS OK"
 fi
@@ -13,5 +14,5 @@ dynamic_ip=$(ping -c 1 -W 10 "$ip_address" | awk -F'[()]' '/PING/{print $2}')
 echo "NewIP: $dynamic_ip"
 
 if grep -q "nfs" /etc/fstab; then
-    sed -i "s/^[[:space:]]*[0-9.]\+\s*:/$dynamic_ip:/" /etc/fstab
+    sed -i "s/^[[:space:]]*[0-9.]\+:/ $dynamic_ip:/" /etc/fstab
 fi
