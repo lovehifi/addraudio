7166b2effc8da976952b7f5c3db930ab316bd617
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-10-09 15:52:03 +0700

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -4,15 +4,21 @@
 if ! grep -q "nfs" /etc/fstab; then
     echo "Adding default nfs line to /etc/fstab"
     sed -i '$!N;/\n$/!P;D' /etc/fstab # Remove trailing empty lines
-    echo -e "\n11.22.33.44:/DSD  /mnt/MPD/NAS/DSD  nfs   defaults,noauto,bg,soft,timeo=5  0  0" >> /etc/fstab
+    echo -e "\n11.22.33.44:/DSD  /mnt/MPD/NAS/DSD  nfs   defaults,noauto,bg,soft,timeo=5  0  0\n" >> /etc/fstab
 else
     echo "NFS OK"
 fi
 
 ip_address=$(cat /boot/ip.ini)
-dynamic_ip=$(ping -c 1 -W 10 "$ip_address" | awk -F'[()]' '/PING/{print $2}')
-echo "NewIP: $dynamic_ip"
+ping_result=$(ping -c 1 -W 10 "$ip_address")
 
-if grep -q "nfs" /etc/fstab; then
-    sed -i "s/^[[:space:]]*[0-9.]\+:/ $dynamic_ip:/" /etc/fstab
+if [[ $? -eq 0 ]]; then
+    dynamic_ip=$(echo "$ping_result" | awk -F'[()]' '/PING/{print $2}')
+    echo "NewIP: $dynamic_ip"
+
+    if grep -q "nfs" /etc/fstab; then
+        sed -i "s/^[[:space:]]*[0-9.]\+:/ $dynamic_ip:/" /etc/fstab
+    fi
+else
+    echo "Unable to ping $ip_address. Skipping IP replacement."
 fi
