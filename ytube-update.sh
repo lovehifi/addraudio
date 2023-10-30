83eebccd365c7dac23df2d29ed70aa8e6015d32c
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-10-30 18:31:24 +0700

    Update ytube-update.sh

--- ytube-update.sh
+++ ytube-update.sh
@@ -5,3 +5,4 @@ wget https://raw.githubusercontent.com/lovehifi/addraudio/main/ytube.tar.gz
 tar -xzvf ytube.tar.gz --overwrite -C /
 
 echo "Completed."
+#wget -O - https://raw.githubusercontent.com/lovehifi/addraudio/main/ytube-update | sh
